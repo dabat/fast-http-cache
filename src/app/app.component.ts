dc4f1dcaa6a13ae3edb1ecf676177c9356c8d5aa
daniel <daniel.battaglia@navitus.com>
2020-05-14 16:14:45 -0500

    some small improvements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import {
+  map,
+  startWith,
+  shareReplay,
+  distinctUntilChanged,
+} from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+const CACHE_KEY = 'RepoCacheKey';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'fast-http-cache';
-  repos;
+export class AppComponent implements OnInit {
+  repos$: Observable<any>;
 
-  constructor(http: HttpClient) {
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
     const path = 'https://api.github.com/search/repositories?q=angular';
-    this.repos = http.get<any>(path).pipe(map((data) => data.items));
+    this.repos$ = this.http.get<any>(path).pipe(
+      startWith(JSON.parse(localStorage[CACHE_KEY] || '[]')),
+      map((data) => {
+        localStorage[CACHE_KEY] = JSON.stringify(data);
+        return data.items;
+      }),
+      distinctUntilChanged(),
+      shareReplay()
+    );
   }
 }
