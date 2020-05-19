8189810bd1e4d8b954c1d483f966ac1bd2c6f392
daniel <daniel.battaglia@navitus.com>
2020-05-18 22:42:09 -0500

    adding original tutorial version for reference

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {
-  map,
-  startWith,
-  shareReplay,
-  distinctUntilChanged,
-} from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 
 const CACHE_KEY = 'RepoCacheKey';
 
@@ -15,21 +9,19 @@ const CACHE_KEY = 'RepoCacheKey';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  repos$: Observable<any>;
+export class AppComponent {
+  repos;
 
-  constructor(private http: HttpClient) {}
-
-  ngOnInit() {
+  constructor(private http: HttpClient) {
     const path = 'https://api.github.com/search/repositories?q=angular';
-    this.repos$ = this.http.get<any>(path).pipe(
-      startWith(JSON.parse(localStorage[CACHE_KEY] || '[]')),
-      map((data) => {
-        localStorage[CACHE_KEY] = JSON.stringify(data);
-        return data.items;
-      }),
-      distinctUntilChanged(),
-      shareReplay()
+    this.repos = this.http.get<any>(path).pipe(map((data) => data.items));
+
+    this.repos.subscribe((next) => {
+      localStorage[CACHE_KEY] = JSON.stringify(next);
+    });
+
+    this.repos = this.repos.pipe(
+      startWith(JSON.parse(localStorage[CACHE_KEY] || '[]'))
     );
   }
 }
